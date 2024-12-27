0f3239ce1646a619a4c481fa3b1a3c94fcd5f056
tomurashigaraki22 <emmanuelhudson355@gmail.com>
2024-12-27 22:32:30 +0100

    fixing bug

--- vite.config.js
+++ vite.config.js
@@ -1,21 +1,21 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-import { resolve } from 'path';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { resolve } from "path";
 
 export default defineConfig({
   plugins: [react()],
   build: {
     lib: {
-      entry: resolve(__dirname, 'src/index.jsx'),
-      name: 'ModernReactTable',
-      fileName: (format) => `modern-react-table.${format}.jsx`
+      entry: resolve(__dirname, "src/index.jsx"),
+      name: "ModernReactTable",
+      fileName: (format) => `modern-react-table.${format}.js` // Change .jsx to .js
     },
     rollupOptions: {
-      external: ['react', 'react-dom'],
+      external: ["react", "react-dom"],
       output: {
         globals: {
-          react: 'React',
-          'react-dom': 'ReactDOM'
+          react: "React",
+          "react-dom": "ReactDOM"
         }
       }
     }
